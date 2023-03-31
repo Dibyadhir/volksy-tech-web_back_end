892e2188912c20569e849cf982d4b99416a017a5
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-03-31 06:28:35 +0000

    task-4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,14 @@
 const http = require('http');
+
 const hostname = 'localhost';
 const port = 1245;
 
-const app = http.createSrever((req, res) => {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('content-Type', 'text/plain');
-  res.end('Hello Holberton School');
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
+
 app.listen(port, hostname, () => {
 
 });
