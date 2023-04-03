f093f89a15304b1c7ed886100ae4e7e8e8b54d5b
Dibyadhir <dibyakantid@brightcomgroup.com>
2023-04-03 06:30:35 +0000

    task-5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+
+const countStudents = require('./3-read_file_async.js');
+
+const db = process.argv[2];
+
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(db);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+app.listen(port, hostname, () => {
+
+});
+module.exports = app;
